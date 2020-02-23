543aa7be258226b88053caa0386f4ae5de7693cb
ajioka-fumito <ajioka.ds@gmail.com>
2020-02-23 12:58:54 +0900

    Initial Commit

---
+++ timer-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  timer-app
+//
+//  Created by Ajioka Fumito on 2020/02/23.
+//  Copyright © 2020 Ajioka Fumito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
